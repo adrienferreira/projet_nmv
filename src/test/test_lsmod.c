cc39026737abd4303b81d4d52e9ed88afa6ed8bc
Redha Gouicem <redha.gouicem@gmail.com>
2015-12-28 17:24:33 +0100

    lsmod ioctl test program

---
+++ src/test/test_lsmod.c
@@ -0,0 +1,45 @@
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "structs.h"
+
+int main(int argc, char **argv)
+{
+	int f;
+	struct lsmod_struct *res, *cur;
+
+	if (argc != 1) {
+		printf("Usage : %s\n", argv[0]);
+		return 1;
+	}
+
+	f = open("/dev/"CHRDEV_NAME, S_IWUSR);
+
+	if (f == -1) {
+		perror("Open");
+		return 1;
+	}
+
+	res = malloc(getpagesize());
+	cur = res;
+
+	ioctl(f, LSMOD_IOCTL, res);
+
+	printf("Name\t\t\tSize  Used by\n");
+	int i = 0;
+	while (cur->size != 0) {
+		printf("%-24s%-5u %u\n", cur->name, cur->size, cur->ref);
+		cur++;
+		i++;
+	}
+	
+	free(res);
+	close(f);
+
+	return 0;
+}
