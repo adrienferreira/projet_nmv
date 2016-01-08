3b661a255a6a8dc1a5660ed08767d95a47525012
AERREIRA ADRIEN <3405623@ppti-14-508-15.ufr-info-p6.jussieu.fr>
2016-01-08 18:11:32 +0100

    Shell can now handle wait and waitall commands.

--- src/shellghoumi/shellghoumi.c
+++ src/shellghoumi/shellghoumi.c
@@ -196,12 +196,48 @@ int perform_print_cpuinfo(int fd)
 	return 0;
 }
 
+void wait_build_struct(int argc, char*argv[], struct gen_wait_usr_struct *gwus)
+{
+	int i;
+
+	gwus->nb_pid = (argc - 1);
+	gwus->pids=(pid_t*)malloc(gwus->nb_pid * sizeof(pid_t));
+
+	if(!(gwus->pids)){
+		perror("PIDs allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for(i=0; i < gwus->nb_pid; i++)
+		gwus->pids[i] = (pid_t)atoi(argv[i+1]);
+}
+
 int perform_wait(int argc, char **argv, int fd)
 {
-	return 0;
+	struct gen_wait_usr_struct gwus; 
+	
+	if(argc == 1)
+	{
+		printf("Usage : ./prog <pid1>, ..., <pidN>\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	wait_build_struct(argc, argv, &gwus);
+	return ioctl(fd, WAIT_IOCTL, &gwus);
 }
 
 int perform_waitall(int argc, char **argv, int fd)
 {
-	return 0;
+	struct gen_wait_usr_struct gwus; 
+	
+	if(argc == 1)
+	{
+		printf("Usage : ./prog <pid1>, ..., <pidN>\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	wait_build_struct(argc, argv, &gwus);
+	return ioctl(fd, WAITALL_IOCTL, &gwus);
 }
+
+
