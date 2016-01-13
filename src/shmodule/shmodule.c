30962aa29a3a3b444ee27de648ce7a0133c911f2
adrienferreira <adrien_ferreira@yahoo.fr>
2016-01-13 18:23:36 +0100

    Added pending result list (work in progress).

--- src/shmodule/shmodule.c
+++ src/shmodule/shmodule.c
@@ -9,6 +9,7 @@
 #include <linux/mutex.h>
 #include <linux/mm.h>
 #include <linux/swap.h>
+#include <asm-generic/errno-base.h>
 
 #include "structs.h"
 #include "shmodule.h"
@@ -26,8 +27,25 @@ static struct file_operations fops = {
 	.unlocked_ioctl = perform_ioctl
 };
 
+struct pend_result{	
+	struct list_head list;
+	unsigned long id_pend;
+	void* data;
+	bool done;
+};
+
+static DEFINE_MUTEX(mutex_pend_results);
+static struct list_head lst_pend_results;
+static long cur_id_pend;
+
+static long add_pending_result(void);
+static struct pend_result* get_result(long id);
+
 static int shmodule_init(void)
-{
+{	
+	INIT_LIST_HEAD(&lst_pend_results);
+	cur_id_pend = 0;
+
 	major_nr = register_chrdev(0, CHRDEV_NAME, &fops);
 
 	if (major_nr == -EINVAL || major_nr == -EBUSY){
@@ -49,8 +67,7 @@ static void shmodule_exit(void)
 }
 module_exit(shmodule_exit);
 
-static long perform_ioctl(struct file *filp, unsigned int cmd,
-			   unsigned long arg)
+static long perform_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	switch (cmd) {
 	case KILL_IOCTL:
@@ -66,4 +83,38 @@ static long perform_ioctl(struct file *filp, unsigned int cmd,
 	default:
 		return -ENOTTY;
 	}
+}
+
+static long add_pending_result()
+{
+	struct pend_result *pr;
+	
+	pr = (struct pend_result*)kmalloc(sizeof(struct pend_result), GFP_KERNEL);
+
+	if (pr == NULL){
+		pr_warn("Impossible to allocate space to store the result");
+		return -ENOMEM;
+	}
+
+	INIT_LIST_HEAD(&(pr->list));
+	pr->data=NULL;
+	pr->done = false;
+
+	mutex_lock(&mutex_pend_results);
+	pr->id_pend = cur_id_pend++;
+	list_add_tail(&(pr->list), &lst_pend_results);
+	mutex_unlock(&mutex_pend_results);
+
+	return pr->id_pend;
+}
+
+static struct pend_result* get_result(long id)
+{
+	struct pend_result *pr;
+
+	list_for_each_entry(pr, &lst_pend_results, list)
+		if (pr->id_pend == id)
+			return pr;
+	
+	return NULL;
 }
