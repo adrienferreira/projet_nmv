169428d42fca8bbe74b93b606c076465392460a0
Redha Gouicem <redha.gouicem@gmail.com>
2016-01-15 15:16:03 +0100

    moved struct and function declarations to header

--- src/shmodule/shmodule.h
+++ src/shmodule/shmodule.h
@@ -1,6 +1,18 @@
 #ifndef SHMODULE_H_
 #define SHMODULE_H_
 
+struct pend_result {
+	struct list_head list;
+	unsigned long id_pend;
+	void* data;
+	bool done;
+};
+
+
 static long perform_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
+static struct pend_result* add_pending_result(void);
+
+static struct pend_result* get_result(long id);
 
 #endif
